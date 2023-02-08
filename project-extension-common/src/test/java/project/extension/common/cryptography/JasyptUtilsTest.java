b7f52308f6498d7b277d7c3ab75bfaa5747c3534
Lc3586 <2445624023@qq.com>
2023-02-08 16:11:23 +0800

    ~

--- project-extension-common/src/test/java/project/extension/common/cryptography/JasyptUtilsTest.java
+++ project-extension-common/src/test/java/project/extension/common/cryptography/JasyptUtilsTest.java
@@ -60,4 +60,42 @@ public class JasyptUtilsTest {
 
         System.out.println("测试已通过");
     }
+
+    /**
+     * 加密
+     */
+    @Test
+    @DisplayName("加密")
+    public void encrypt() {
+        String password = JasyptUtils.getPassword();
+        String text = "123456";
+        System.out.printf("明文：%s%n",
+                          text);
+        String data1 = JasyptUtils.encrypt(text,
+                                           password);
+        System.out.printf("密文1：%s%n",
+                          data1);
+        String data2 = String.format("ENC(%s)",
+                                     data1);
+        System.out.printf("密文2：%s%n",
+                          data2);
+    }
+
+    /**
+     * 解密
+     */
+    @Test
+    @DisplayName("解密")
+    public void decrypt()
+            throws
+            Throwable {
+        String password = JasyptUtils.getPassword();
+        String text = "ENC(kSMyRaceHTrcWti5p607CA==)";
+        System.out.printf("密文：%s%n",
+                          text);
+        String data1 = JasyptUtils.decrypt(text,
+                                           password);
+        System.out.printf("明文：%s%n",
+                          data1);
+    }
 }
