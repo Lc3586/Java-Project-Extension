ad1ab1d57dbd189c1db47073cc0b2a7876f9412d
Lc3586 <2445624023@qq.com>
2023-08-30 17:33:10 +0800

    更新maven配置以发布代码至中心库

--- project-extension-common/src/main/java/project/extension/cryptography/Base64Utils.java
+++ project-extension-common/src/main/java/project/extension/cryptography/Base64Utils.java
@@ -5,7 +5,7 @@ import org.apache.commons.codec.binary.Base64;
 import java.nio.charset.Charset;
 
 /***
- * @Description :base64加密
+ * base64加密
  * @author : cuikefeng
  * @date : 2021/12/13 16:44
  **/
@@ -16,8 +16,6 @@ public class Base64Utils {
 
     /***
      * 编码
-     * @param data
-     * @return
      **/
     public static String encode(String data) {
         return new Base64().encodeToString(data.getBytes());
@@ -25,8 +23,6 @@ public class Base64Utils {
 
     /***
      * 编码返回字符串
-     * @param key
-     * @return
      **/
     public static String encodeBase64(byte[] key) {
         return new Base64().encodeToString(key);
@@ -34,18 +30,18 @@ public class Base64Utils {
 
     /***
      * 编码返回字符串
-     * @param key
      * @param charset 字符集
-     * @return
      **/
-    public static String encodeBase64(byte[] key, Charset charset) {
-        return key == null ? null : new String(new Base64().encode(key), charset);
+    public static String encodeBase64(byte[] key,
+                                      Charset charset) {
+        return key == null
+               ? null
+               : new String(new Base64().encode(key),
+                            charset);
     }
 
     /***
      * 解码
-     * @param data
-     * @return
      **/
     public static String decode(String data) {
         return new String(new Base64().decode(data));
@@ -53,18 +49,17 @@ public class Base64Utils {
 
     /***
      * 解码
-     * @param data
      * @param charset 字符集
-     * @return
      **/
-    public static String decode(String data, Charset charset) {
-        return new String(decodeBase64(data, charset), charset);
+    public static String decode(String data,
+                                Charset charset) {
+        return new String(decodeBase64(data,
+                                       charset),
+                          charset);
     }
 
     /***
      * 解码
-     * @param data
-     * @return
      **/
     public static byte[] decodeBase64(String data) {
         return new Base64().decode(data);
@@ -72,18 +67,15 @@ public class Base64Utils {
 
     /***
      * 解码
-     * @param data
      * @param charset 字符集
-     * @return
      **/
-    public static byte[] decodeBase64(String data, Charset charset) {
+    public static byte[] decodeBase64(String data,
+                                      Charset charset) {
         return new Base64().decode(data.getBytes(charset));
     }
 
     /***
      * 解码
-     * @param data
-     * @return
      **/
     public static byte[] decodeBase64(byte[] data) {
         return new Base64().decode(data);
